8907d37baeb11da577c747ed8379a7ba45201b82
Marcus Vinicius <m.viniciuz@hotmail.com>
2019-09-11 22:53:11 -0300

    fix: Listando Agenda do prestador!

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,41 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointment = await Appointment.findAll({
+      where: {
+        user_id: req.userId,
+        canceled_at: null,
+      },
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointment);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -18,22 +51,46 @@ class AppointmentController {
      */
     const { provider_id, date } = req.body;
 
-    const isprovider = await User.findOne({
+    const checkisProvider = await User.findOne({
       where: {
         id: provider_id,
         provider: true,
       },
     });
-    if (!isprovider) {
+    if (!checkisProvider) {
       return res
         .status(401)
         .json({ error: 'Campo providers_id com provider inválido!' });
     }
 
+    /**
+     * Check for past dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Data não permitida' });
+    }
+
+    /**
+     * check date availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Essa Data não está disponível' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 import AppointmentController from './app/controllers/AppointmentController';
@@ -22,7 +23,12 @@ routes.use(authMiddleware);
 routes.post('/files', upload.single('file'), FileController.store);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
-routes.post('/Appointments', AppointmentController.store);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedules', ScheduleController.index);
 
 export default routes;
