44b5d4248cf9dbbc4a3f27810ac9133fd3bb5d78
Marcus Vinicius <m.viniciuz@hotmail.com>
2019-09-16 22:03:01 -0300

    fix: Configurando o nodemailer!

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -7,6 +7,8 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
+import Mail from '../../lib/mail';
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -123,7 +125,15 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
       return res.json({
@@ -141,6 +151,12 @@ class AppointmentController {
 
     appointment.canceled_at = new Date();
     appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento Cancelado',
+      text: 'Você tem um novo cancelamento!',
+    });
 
     return res.json(appointment);
   }
---
+++ src/config/mail.js
@@ -0,0 +1,21 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '42404c7d88cee2',
+    pass: 'b19215241c3511',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe Gobarber <noreplay@gmail.com>',
+  },
+};
+
+/**
+ * Serviços de e-mail que podem ser usados para configuração
+ * amazon SES
+ * mailgun
+ * Sparkpost
+ * Mandril (MailClimb)
+ *
+ */
